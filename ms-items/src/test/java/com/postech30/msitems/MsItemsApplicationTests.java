c33c200235d6b766259d22a4352489b695c4068f
Victor Toledo <victorptoledo@outlook.com>
2024-02-09 13:35:58 -0300

    Adding ms-items

---
+++ ms-items/src/test/java/com/postech30/msitems/MsItemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postech30.msitems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsItemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
