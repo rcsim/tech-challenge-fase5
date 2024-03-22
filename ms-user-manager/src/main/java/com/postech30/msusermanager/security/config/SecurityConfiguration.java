91d4fe3125d6737ae832a403617459148060779b
Geovane Silveira <geovanedsilveira@gmail.com>
2024-03-21 21:22:18 -0300

    Implemented security config

--- ms-user-manager/src/main/java/com/postech30/msusermanager/security/config/SecurityConfiguration.java
+++ ms-user-manager/src/main/java/com/postech30/msusermanager/security/config/SecurityConfiguration.java
@@ -2,8 +2,17 @@ package com.postech30.msusermanager.security.config;
 
 import com.postech30.msusermanager.security.authentication.UserAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -28,4 +37,31 @@ public class SecurityConfiguration {
             "/users/test/administrator"
     };
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)
+        throws Exception {
+            return httpSecurity
+                    .csrf().disable()
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and().authorizeHttpRequests()
+                    .requestMatchers(ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED).permitAll()
+                    .requestMatchers(ENDPOINTS_WITH_AUTHENTICATION_REQUIRED).authenticated()
+                    .requestMatchers(ENDPOINTS_ADMIN).hasRole("ADMINISTRATOR")
+                    .requestMatchers(ENDPOINTS_CUSTOMER).hasRole("CUSTOMER")
+                    .anyRequest().denyAll()
+                    .and().addFilterBefore(userAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                    .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+        throws Exception {
+            return authenticationConfiguration.getAuthenticationManager();
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
