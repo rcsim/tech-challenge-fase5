b811c7fca0e6e06eab8897b20b280f93c9801086
Geovane Silveira <geovanedsilveira@gmail.com>
2024-03-25 11:22:55 -0300

    Adjusted properties and User entity

--- ms-user-manager/src/main/java/com/postech30/msusermanager/entity/User.java
+++ ms-user-manager/src/main/java/com/postech30/msusermanager/entity/User.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String email;
     private String password;
